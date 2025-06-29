654c3a231e5323e91ceb6b85741caf34dac1b155
MJ LEE <sclass0614@gmail.com>
2025-06-29 11:12:25 +0900

    복구

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', function(e) {
-  console.log('Service Worker 설치됨');
-  self.skipWaiting(); // 바로 활성화
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
-});
